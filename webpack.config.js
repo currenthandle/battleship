10a54765257a4612919b08f45637f8bf8f49870c
Casey <leptone55@gmail.com>
2017-09-27 16:11:22 -0700

    Auotprefixer in build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,77 @@
-'use strict'
-
-const path = require('path')
-
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-const webpack = require('webpack')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ETP = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+	context: path.join(__dirname, 'src'),
+	entry: path.join(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js'
     },
-    module: {
-        rules: [
-            { 
-                test: /\.js$/, 
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react', 'stage-0'],
-                        plugins: [ 'transform-runtime' ]
-                    }
-                }
-            },
-			{
-				test: /\.json$/,
-				loader: 'json-loader'
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: "style-loader",
-					use: {
-                        loader: 'css-loader',
-                    }
-				})
-			},
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [ 
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                importLoaders: 1
-                            }
-                        }, 
-                        'sass-loader'
-                    ]
-				})
-			},
-            {
-                test: /(.png|jpg|jpeg|webp)$/,
-                use: [
-                    {
-                        loader: 'file-loader'
-                    }
-                ]
-            },
+	module: {
+		loaders: [
+		{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			include: path.join(__dirname, 'src'),
+            options: {
+                presets: ['env', 'react', 'stage-0'],
+                plugins: [ 'transform-runtime' ]
+            }
+		},
+		{
+			test: /\.(css|scss|sass)$/,
+			// loader: ETP.extract('style-loader', 'css-loader!sass-loader'),
+			loader: ETP.extract({
+				fallback: 'style-loader',
+				use: 'css-loader!postcss-loader!sass-loader'
+			}),
+			include: path.join(__dirname, 'src')
+		},
+		{
+			test: /\.(png|jpg)$/,
+			loader: 'file-loader',
+			include: path.join(__dirname, 'src')
+		}
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		inline: true,
+		hot: true,
+		historyApiFallback: true,
+	},
+	resolve: {
+		alias: {
+			'styles': path.resolve(__dirname, 'src/stylesheets'),
+			'images': path.resolve(__dirname, 'src/assets')
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src', 'index.html'),
+			inject: 'body',
+			excludeChunks: ['about'],
+			filename: 'index.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src', 'index.html'),
+			inject: 'body',
+			excludeChunks: ['home'],
+			filename: 'about.html'
+		}),
+		new webpack.HotModuleReplacementPlugin(),
 
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'dist/index.html'
-        }),
-        new ExtractTextPlugin('style.css')
-    ],
-    devServer: {
-        historyApiFallback: true
-    }
-} 
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common'
+		}),
+
+		// new ETP('styles.css'),
+		new ETP({
+			filename: '[name].styles.css',
+			allChunks: true
+		}),
+		]
+};
